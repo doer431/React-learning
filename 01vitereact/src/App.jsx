330a6de50aff4c5fed83f82429a632e4665c20da
doer431 <shuraimfg@gmail.com>
2025-07-28 19:56:23 +0530

    first component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,11 @@
 
+import Chai from "./Chai"
 function App() {
   return (
-    <h1>Chai aur react with vite | Shuraim</h1>
+    <>
+      <Chai/>
+      <h1>Chai aur react with vite | Shuraim</h1>
+    </>
   )
 }
 
