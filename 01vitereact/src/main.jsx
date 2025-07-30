c5bfaf992b66d551f6a5d759a48d3a790297e510
doer431 <shuraimfg@gmail.com>
2025-07-30 16:29:59 +0530

    React.createElement

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { createElement } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// const reactElement = {
+//     type : "a",
+//     props :{
+//         href :"https://google.com",
+//         target :"_blank"
+//     },
+//     Children : " click me"
+// }
+
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">click me to vist google</a>
+// )
+
+// const reactElement = React.createElement(
+//   'a',
+//   {href : 'https://google.com', target : '_blank'},
+//   'click to vist google | shuraim'
+// )
+
+function MyApp(){
+  return(
+    <div>
+      <h2>chai aur react | shuraim</h2>
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // anotherElement
+  // reactElement
+  // myApp()
+  <MyApp/>
 );
