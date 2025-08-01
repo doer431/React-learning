d9908fe3d57f9434511c6677a587cc0d1f56a26e
doer431 <shuraimfg@gmail.com>
2025-08-01 15:07:55 +0530

    userState implementation

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+let [count , setCount] = useState(15)
+
+  // let count = 0
+
+  // const check = () =>{
+  //   if(count == 0){
+  //     alert("0 se nechhe nahi jaye ga be");
+  //     return 
+  //   }else if(count == 20){
+  //     alert("20 se uppar nahi jaye ga......");
+  //     return 
+  //   }
+  // }
+  const addValue = () => {
+    if (count ==20 ){
+      alert("20 se uppar nahi jaye ga......");
+      return
+    }
+    setCount(count + 1);
+    }
+  const removeValue = () =>{
+    if( count == 0){
+      alert("0 se nechhe nahi jaye ga be");
+      return
+    }
+    setCount( count - 1)
+  }
+
+  return (
+    <>
+    <h2> Chai aur react | userState</h2>
+    <p> counter value {count}</p>
+    <button onClick={addValue}>Add value {count} </button>  
+    <button onClick={removeValue} >Remove value  {count} </button>
+    </>
+    // setCount(count + 1)
+  )
+}
+
+export default App
